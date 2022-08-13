1d4f150971a6e0657cfd7113668e7d10b399b63d
Boris Mühmer <boris.muehmer@gmail.com>
2022-08-13 19:43:13 +0200

    added bsic tests

--- internal/orientation/orientation_test.go
+++ internal/orientation/orientation_test.go
@@ -2,6 +2,39 @@ package orientation
 
 import "testing"
 
-func TestSkip(t *testing.T) {
-	t.Skip("not implemented")
+func TestString(t *testing.T) {
+	for _, v := range []struct {
+		o Orientation
+		s string
+	}{
+		{o: Unknown, s: textUnknown},
+		{o: MxOne, s: textMxOne},
+		{o: OnexN, s: textOnexN},
+		{o: OnexOne, s: textOnexOne},
+		{o: MxN, s: textMxN},
+	} {
+		if s := v.o.String(); s != v.s {
+			t.Errorf("%s.String() is %q, expected %q", v.o, s, v.s)
+		}
+	}
+}
+
+func TestParse(t *testing.T) {
+	for _, v := range []struct {
+		o Orientation
+		s string
+	}{
+		{o: Unknown, s: textUnknown},
+		{o: MxOne, s: textMxOne},
+		{o: OnexN, s: textOnexN},
+		{o: OnexOne, s: textOnexOne},
+		{o: MxN, s: textMxN},
+		{o: Unknown, s: "⨯⨯⨯"},
+		{o: Unknown, s: "xXx"},
+		{o: Unknown, s: "Semmelknödel"},
+	} {
+		if o := Parse(v.s); o != v.o {
+			t.Errorf("Parse(%q) is %s, expected %s", v.o, o, v.s)
+		}
+	}
 }
