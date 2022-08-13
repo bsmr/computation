c97c04c109c3b2ee4ad9e11d1a57783d94d55778
Boris Mühmer <boris.muehmer@gmail.com>
2022-08-13 17:57:28 +0200

    added more tests and fix another bug

--- vector/vector.go
+++ vector/vector.go
@@ -27,9 +27,9 @@ func New[T computation.Numeric](values ...T) (*Vector[T], error) {
 func (v *Vector[T]) At(index int) (T, error) {
 	switch v.layout {
 	case orientation.MxOne:
-		return v.data.At(index, 1)
+		return v.data.At(index, 0)
 	case orientation.OnexN:
-		return v.data.At(1, index)
+		return v.data.At(0, index)
 	default:
 		var dummy T
 		return dummy, fmt.Errorf("cannot handle layout %s", v.layout)
@@ -39,9 +39,9 @@ func (v *Vector[T]) At(index int) (T, error) {
 func (v *Vector[T]) SetAt(index int, newValue T) (T, error) {
 	switch v.layout {
 	case orientation.MxOne:
-		return v.data.SetAt(index, 1, newValue)
+		return v.data.SetAt(index, 0, newValue)
 	case orientation.OnexN:
-		return v.data.SetAt(1, index, newValue)
+		return v.data.SetAt(0, index, newValue)
 	default:
 		var dummy T
 		return dummy, fmt.Errorf("cannot handle layout %s", v.layout)
@@ -67,6 +67,28 @@ func (v *Vector[T]) String() string {
 	return sb.String()
 }
 
+func (v *Vector[T]) Equal(a *Vector[T]) bool {
+	if v.Rank() != a.Rank() {
+		return false
+	}
+	for i := 0; i < v.Rank(); i++ {
+		v1, _ := v.At(i)
+		v2, _ := a.At(i)
+		//if !reflect.DeepEqual(v1, v2) {
+		if v1 != v2 {
+			return false
+		}
+	}
+	return true
+}
+
+func (v *Vector[T]) EqualReal(a *Vector[T]) bool {
+	if v.layout != a.layout {
+		return false
+	}
+	return v.Equal(a)
+}
+
 func Addition[T computation.Numeric](a, b *Vector[T]) (*Vector[T], error) {
 	c, err := container.Addition(a.data, b.data)
 	if err != nil {
@@ -80,6 +102,17 @@ func Addition[T computation.Numeric](a, b *Vector[T]) (*Vector[T], error) {
 
 func Transposition[T computation.Numeric](a *Vector[T]) (*Vector[T], error) {
 	c, err := container.Transposition(a.data)
+	if err != nil {
+		return nil, err
+	}
+	return &Vector[T]{
+		data:   c,
+		layout: orientation.Check(c),
+	}, nil
+}
+
+func ScalarMultiplication[T computation.Numeric](s T, a *Vector[T]) (*Vector[T], error) {
+	c, err := container.ScalarMatrixMultiplication(s, a.data)
 	if err != nil {
 		return nil, err
 	}
--- vector/vector_test.go
+++ vector/vector_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/bsmr/computation/internal/container"
+	"github.com/bsmr/computation/internal/orientation"
 )
 
 func TestOps(t *testing.T) {
@@ -42,4 +43,111 @@ func TestOps(t *testing.T) {
 		t.Fatalf("Add() failed with: %s", err)
 	}
 	t.Logf("r2 = %s", r2)
+}
+
+func TestAddition(t *testing.T) {
+	v1, err := New(1, 2, 3, 4)
+	if err != nil {
+		t.Fatalf("v1.New() failed with: %s", err)
+	}
+	v2, err := New(6, 7, 8, 9)
+	if err != nil {
+		t.Fatalf("v2.New() failed with: %s", err)
+	}
+	vr, err := New(7, 9, 11, 13)
+	if err != nil {
+		t.Fatalf("vr.New() failed with: %s", err)
+	}
+
+	t.Logf("v1 = %s", v1)
+	t.Logf("v2 = %s", v2)
+
+	v3, err := Addition(v1, v2)
+	if err != nil {
+		t.Fatalf("Addition() failed with: %s", err)
+	}
+	t.Logf("v3 = %s", v3)
+
+	if !v3.Equal(vr) {
+		t.Fatalf("v3 is %s, expected %s", v3, vr)
+	}
+}
+
+func TestTransposition(t *testing.T) {
+	vals := []int{1, 2, 3, 4}
+	v1, err := New(vals...)
+	if err != nil {
+		t.Fatalf("v1.New() failed with: %s", err)
+	}
+	data, _ := container.New(1, 4, vals...)
+	vr := &Vector[int]{
+		data:   data,
+		layout: orientation.OnexN,
+	}
+
+	t.Logf("v1 = %s", v1)
+
+	v3, err := Transposition(v1)
+	if err != nil {
+		t.Fatalf("Transposition() failed with: %s", err)
+	}
+	t.Logf("v3 = %s", v3)
+
+	if !v3.EqualReal(vr) {
+		t.Fatalf("v3 is %s, expected %s", v3, vr)
+	}
+	if v3.EqualReal(v1) {
+		t.Fatalf("v3 and v3 should not be equal")
+	}
+}
+
+func TestScalarMultiplication(t *testing.T) {
+	v1, err := New(1, 2, 3, 4)
+	if err != nil {
+		t.Fatalf("v1.New() failed with: %s", err)
+	}
+	s := 3
+	vr, err := New(3, 6, 9, 12)
+	if err != nil {
+		t.Fatalf("vr.New() failed with: %s", err)
+	}
+
+	t.Logf("v1 = %s", v1)
+	t.Logf("s  = %v", s)
+
+	v3, err := ScalarMultiplication(s, v1)
+	if err != nil {
+		t.Fatalf("ScalarMultiplication() failed with: %s", err)
+	}
+	t.Logf("v3 = %s", v3)
+
+	if !v3.Equal(vr) {
+		t.Fatalf("v3 is %s, expected %s", v3, vr)
+	}
+}
+
+func TestEqual(t *testing.T) {
+	v1, err := New(1, 2, 3)
+	if err != nil {
+		t.Fatalf("v1.New() failed with: %s", err)
+	}
+	v2, err := New(1, 2, 4)
+	if err != nil {
+		t.Fatalf("v2.New() failed with: %s", err)
+	}
+	v3, err := New(1, 2, 3, 5)
+	if err != nil {
+		t.Fatalf("v3.New() failed with: %s", err)
+	}
+
+	t.Logf("v1 = %s", v1)
+	t.Logf("v2 = %s", v2)
+	t.Logf("v3 = %s", v3)
+
+	if v1.Equal(v2) {
+		t.Errorf("v1 and v2 are not equal")
+	}
+	if v1.Equal(v3) {
+		t.Errorf("v1 and v3 are not equal")
+	}
 }
