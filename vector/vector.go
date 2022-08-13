ac8160395ba31b11fe9e93ac2a2aa583125ca039
Boris Mühmer <boris.muehmer@gmail.com>
2022-08-13 13:36:00 +0200

    re-org: cleanup of mess

--- vector/vector.go
+++ vector/vector.go
@@ -1,67 +1,31 @@
 package vector
 
 import (
-	"fmt"
-
 	"github.com/bsmr/computation"
 	"github.com/bsmr/computation/internal/common"
 )
 
 type Vector[T computation.Numeric] struct {
-	rank   int
-	values []T
+	data *common.Container[T]
 }
 
-const (
-	minRank = 1
-)
-
 func New[T computation.Numeric](rank int, values ...T) (*Vector[T], error) {
-	if rank < minRank {
-		return nil, fmt.Errorf("rank must be %d or higher", minRank)
-	}
+	data, err := common.New(rank, 1, values...)
 
-	lv := len(values)
-	if rank < lv {
-		return nil, fmt.Errorf("too many values %d for rank %d", lv, rank)
-	}
-
-	vector := &Vector[T]{
-		rank:   rank,
-		values: make([]T, rank),
-	}
-
-	copy(vector.values, values)
-
-	return vector, nil
+	return &Vector[T]{
+		data: data,
+	}, err
 }
 
 func (v *Vector[T]) At(index int) (T, error) {
-	var r T
-
-	if index < 0 {
-		return r, fmt.Errorf("out of bounds: index %d is less than 0", index)
-	}
-	if index > (v.rank - 1) {
-		return r, fmt.Errorf("out of bounds: index %d is greater than %d", index, v.rank)
-	}
-
-	return v.values[index], nil
+	return v.data.At(index, 1)
 }
 
-func (v *Vector[T]) SetAt(index int, newValue T) (oldValue T, err error) {
-	v.values[index], oldValue = newValue, v.values[index]
-	return
-}
-
-func (v *Vector[T]) Copy() (common.Container[T], error) {
-	return New(v.rank, v.values...)
+func (v *Vector[T]) SetAt(index int, newValue T) (T, error) {
+	return v.data.SetAt(index, 1, newValue)
 }
 
 func (v *Vector[T]) Rank() int {
-	return v.rank
-}
-
-func (v *Vector[T]) Values() []T {
-	return v.values
+	m, _ := v.data.Rank()
+	return m
 }
--- vector/vector_test.go
+++ vector/vector_test.go
@@ -3,7 +3,6 @@ package vector
 import (
 	"testing"
 
-	"github.com/bsmr/computation"
 	"github.com/bsmr/computation/internal/common"
 )
 
@@ -27,16 +26,17 @@ func TestOps(t *testing.T) {
 	t.Logf("v3 = %#v", v3)
 
 	//r1, err := Add(v1)
-	//r1, err := Add(v1, v2)
-	r1, err := common.Add[int](v1, v2, v3)
+	r1, err := common.Addition(v1.data, v2.data)
+	//r1, err := common.Add[int](v1, v2, v3)
 	if err != nil {
 		t.Fatalf("Add() failed with: %s", err)
 	}
 	t.Logf("r1 = %#v", r1)
 
 	//r2, err := Mul(v1)
-	//r2, err := Mul(v1, v2)
-	r2, err := common.Mul[int](v1, v2, v3)
+	m2, _ := common.Transposition(v2.data)
+	r2, err := common.MatrixMatrixMultiplication(v1.data, m2)
+	//r2, err := common.Mul[int](v1, v2, v3)
 	if err != nil {
 		t.Fatalf("Add() failed with: %s", err)
 	}
@@ -55,125 +55,4 @@ func TestSpecialCase(t *testing.T) {
 		t.Fatalf("Add() failed with: %s", err)
 	}
 	t.Logf("r2 = %#v", r2)
-}
-
-func bad[T computation.Numeric]() *Vector[T] {
-	v := &Vector[T]{
-		rank:   1,
-		values: []T{1, 2, 3},
-	}
-	return v
-}
-
-func TestErrors(t *testing.T) {
-	if _, err := New(0, 1); err == nil {
-		t.Fatal("New(0, 1) did not return an error")
-	}
-
-	if _, err := common.Add[int](bad[int]()); err == nil {
-		t.Fatal("Add() did not generate an error")
-	}
-
-	for _, c := range [][]common.Container[int]{
-		{nil},
-		{nil, nil},
-		{
-			&Vector[int]{
-				rank:   1,
-				values: []int{1, 2},
-			},
-			nil,
-		},
-		{
-			nil,
-			&Vector[int]{
-				rank:   1,
-				values: []int{3, 4},
-			},
-		},
-		{
-			&Vector[int]{
-				rank:   1,
-				values: []int{1, 2},
-			},
-			nil,
-			&Vector[int]{
-				rank:   1,
-				values: []int{3, 4},
-			},
-		},
-	} {
-		if _, err := common.Add(c...); err == nil {
-			t.Fatalf("Add(%#v) did not generate an error", c)
-		}
-	}
-}
-
-func TestAt(t *testing.T) {
-	v0, _ := New(3, 1, 2, 3)
-
-	for i, c := range v0.values {
-		v, err := v0.At(i)
-		if err != nil {
-			t.Errorf("%#v.At(%d) failed with: %s", v0, i, err)
-		}
-		if v != c {
-			t.Errorf("%#v.At(%d) is %d, expected %d", v0, i, v, c)
-		}
-	}
-
-	for _, c := range []int{
-		-1,
-		3,
-		4,
-	} {
-		if _, err := v0.At(c); err == nil {
-			t.Fatalf("%#v.At(%d) did not return an errror", v0, c)
-		}
-	}
-}
-
-func TestContainerCompatibility(t *testing.T) {
-	v1, _ := New(3, 1, 2, 3)
-	v2, _ := New(3, 4, 5, 6)
-
-	cs := []common.Container[int]{
-		v1,
-		v2,
-	}
-
-	for _, c := range cs {
-		t.Logf("Rank(): %d", c.Rank())
-	}
-}
-
-func equal(as, bs []int) bool {
-	if len(as) != len(bs) {
-		return false
-	}
-	for i, v := range as {
-		if v != bs[i] {
-			return false
-		}
-	}
-
-	return true
-}
-
-func TestValues(t *testing.T) {
-	for _, vs := range [][]int{
-		{1},
-		{1, 2},
-		{1, 2, 3},
-		{1, 2, 3, 4},
-	} {
-		v, err := New(len(vs), vs...)
-		if err != nil {
-			t.Errorf("New(%d,%#v) failed with: %s", len(vs), vs, err)
-			continue
-		}
-		if rs := v.Values(); !equal(rs, vs) {
-			t.Errorf("Values() is %#v, expected %#v", rs, vs)
-		}
-	}
 }
